2dfa0bda6582c8de06f19c01857b15b226819e75
Manasa Kurella <your.email@example.com>
2024-10-26 10:08:45 +0530

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Redirect } from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 // import  Link  from '@docusaurus/Link';
            
 
 // Default export for Home component
 export default function Home(): JSX.Element {
-  return <Redirect to="/guides/Overview" />;
+  return <Redirect to={useBaseUrl('/guides/Overview')}  />;
 }
